ce37bf7dd15ab281d3ce3b95e07133c6a195ec68
Darsini-Vijayakumar <130814531+Darsini-Vijayakumar@users.noreply.github.com>
2023-10-12 15:48:09 -0400

    Delete restaurant_analysis.sql

--- restaurant_analysis.sql
+++
@@ -1,9 +0,0 @@
-
---SELECT * FROM `sustained-node-382818.Restaurants.Restaurants` LIMIT 5;
---SELECT * FROM `sustained-node-382818.Restaurants.Cuisine` LIMIT 5;
---SELECT * FROM `sustained-node-382818.Restaurants.Ratings` LIMIT 5;
---SELECT * FROM `sustained-node-382818.Restaurants.Payment` LIMIT 100;
---SELECT * FROM `sustained-node-382818.Restaurants.Parking` LIMIT 5;
-...
-
--- rest of the code
