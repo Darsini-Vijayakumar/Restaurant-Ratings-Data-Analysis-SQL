2e5ea6263297653d91c390e030e20a9cc2d4970a
Darsini-Vijayakumar <130814531+Darsini-Vijayakumar@users.noreply.github.com>
2023-10-12 15:47:43 -0400

    Add files via upload

---
+++ restaurant_analysis.sql
@@ -0,0 +1,9 @@
+
+--SELECT * FROM `sustained-node-382818.Restaurants.Restaurants` LIMIT 5;
+--SELECT * FROM `sustained-node-382818.Restaurants.Cuisine` LIMIT 5;
+--SELECT * FROM `sustained-node-382818.Restaurants.Ratings` LIMIT 5;
+--SELECT * FROM `sustained-node-382818.Restaurants.Payment` LIMIT 100;
+--SELECT * FROM `sustained-node-382818.Restaurants.Parking` LIMIT 5;
+...
+
+-- rest of the code
